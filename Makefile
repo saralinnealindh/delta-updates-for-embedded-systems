ae95370150deda2f16db8d3f63fd027b0296c0a0
Linnéa Lindh <linnea.lindh@endian.se>
2020-12-03 22:50:10 +0100

    added some code and cleaned it up

--- Makefile
+++ Makefile
@@ -1,13 +1,36 @@
 BOARD := nrf52840dk_nrf52840
 BOOT_PATH := bootloader/mcuboot/boot/zephyr
 
-all: img1 flash1
+all: bb fb b1 f1
 
-upgrade: img2 flash2
+help:
+	@echo "Make commands that may be utilized:"	
+	@echo "all	build bootloader, flash bootloader, build firmware image and flash firmware image"
+	@echo "1	build and flash firmware image"
+	@echo "2	build and flash upgrade firmware image to secondary slot"
+	@echo "b	build and flash bootloader and erase flash memory"
+	@echo "p	create a patch based on 1 and 2 and append "NEWPATCH" to start of it" 
+	@echo "		then flash this patch to the storage partition."
+	@echo "b1	build the firmware image"		
+	@echo "b2	build the upgrade firmware image"
+	@echo "bb	build the bootloader"
+	@echo "f1	flash the firmware image"
+	@echo "f2	flash the upgrade"		
+	@echo "fb	erase the flash and flash the bootloader"
+	@echo "fp	flash the patch to the storage partition"
+	@echo "cp	create a patch based on the firmware image and the upgraded firmware image and"
+	@echo "		append NEWPATCH to the beginning of it"
+	@echo "c	connect to the device terminal"		
 
-boot: build-boot flash-boot
+1: b1 f1
 
-img1: 
+2: b2 f2
+
+b: bb fb
+
+p: cp fp
+
+b1: 
 	mkdir -p zephyr/build 
 	mkdir -p images
 	west build -p auto -b $(BOARD) -d zephyr/build app	
@@ -22,7 +45,7 @@ img1:
 		--align 4 \
 		--key bootloader/mcuboot/root-rsa-2048.pem 
 	
-img2:
+b2:
 	mkdir -p zephyr/build 
 	mkdir -p imagesg
 	west build -p auto -b $(BOARD) -d zephyr/build upgrade_app
@@ -38,27 +61,28 @@ img2:
 		--pad \
 		--key bootloader/mcuboot/root-rsa-2048.pem 
 	
-flash1: 
+f1: 
 	pyocd flash -e sector -a 0x0c000 -t nrf52840 images/signed1.hex
 	
-flash2: 
+f2: 
 	pyocd flash -e sector -a 0x73000 -t nrf52840 images/signed2.hex
 
-flashB:
+fb:
 	ninja -C $(BOOT_PATH)/build flash
 
-flashP:
-	pyocd flash -e sector -a 0xf8000 -t nrf52840 patches/patch.bin
+fp:
+	pyocd flash -e sector -a 0xf8000 -t nrf52840 patches/magic-patch.bin
 
-build-boot:
+bb:
 	mkdir -p $(BOOT_PATH)/build
 	cmake -B $(BOOT_PATH)/build -GNinja -DBOARD=BOARD -S $(BOOT_PATH)
 	ninja -C $(BOOT_PATH)/build
 	
-create-patch:
+cp:
 	mkdir -p patches
-	rm -f patches/test.patch
+	rm -f patches/patch.bin
 	detools create_patch images/signed1.hex images/signed2.hex patches/patch.bin
+	printf "NEWPATCH" | cat - patches/patch.bin > patches/magic-patch.bin
 	
-connect:
+c:
 	JLinkRTTLogger -device NRF52 -if SWD -speed 5000 -rttchannel 0 /dev/stdout
--- app/src/test2.c
+++ app/src/test2.c
@@ -6,9 +6,15 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <sys/util.h>
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include <drivers/flash.h>
+#include <inttypes.h>
+#include <storage/flash_map.h>
+#include <stdio.h>
+
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -28,28 +34,110 @@
 #define FLAGS	0
 #endif
 
+/*
+ * Get button configuration from the devicetree sw0 alias.
+ *
+ * At least a GPIO device and pin number must be provided. The 'flags'
+ * cell is optional.
+ */
+
+#define SW0_NODE	DT_ALIAS(sw0)
+
+#if DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#define SW0_GPIO_LABEL	DT_GPIO_LABEL(SW0_NODE, gpios)
+#define SW0_GPIO_PIN	DT_GPIO_PIN(SW0_NODE, gpios)
+#define SW0_GPIO_FLAGS	(GPIO_INPUT | DT_GPIO_FLAGS(SW0_NODE, gpios))
+#else
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#define SW0_GPIO_LABEL	""
+#define SW0_GPIO_PIN	0
+#define SW0_GPIO_FLAGS	0
+#endif
+
+// STORAGE 
+#define STORAGE_OFFSET FLASH_AREA_OFFSET(storage)
+
+static struct gpio_callback button_cb_data;
+static bool btn_flag; 
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins);
+
+bool configure_devices(const struct device *flash, const struct device *button, const struct device *led);
+
+
 void main(void)
 {
-	struct device *dev;
-	bool led_is_on = true;
-	int ret;
+	const struct device *led,*button,*flash;
+	int ret,length;
+	bool led_is_on;
+
+	led = device_get_binding(LED0);
+	button = device_get_binding(SW0_GPIO_LABEL);
+	flash = device_get_binding(DT_CHOSEN_ZEPHYR_FLASH_CONTROLLER_LABEL);
+
+	length = 8;
+	led_is_on = true;
+	btn_flag = false; 
+
+	unsigned char buf[length];
+
+	if (!led||!button||!flash)
+		return;
+
+	if (!configure_devices(flash,button,led))
+		return;
 	
-	printk("Hello World from %s on %s!\n",
-	MCUBOOT_BLINKY2_FROM, CONFIG_BOARD);
+	printk("Hello World from %s on %s!\n",MCUBOOT_BLINKY2_FROM, CONFIG_BOARD);
 
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
-		return;
-	}
-
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret < 0) {
-		return;
-	}
-
+	/*Main loop*/
 	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
+		gpio_pin_set(led, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
+
+		if(btn_flag){
+			// Read from memory
+			if(!flash){
+				return;
+			}
+
+			ret = flash_read(flash,STORAGE_OFFSET, &buf,(length+1)*sizeof(char));
+
+			if(ret!=0){
+				return;
+			}
+
+			printk("Word: %s\n",buf);
+
+			btn_flag = false;
+		}
 	}
+}
+
+/*Exectues when button is pressed*/
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	btn_flag = true;
+}
+
+bool configure_devices(const struct device *flash, const struct device *button, const struct device *led) {
+	int ret; 
+	ret = gpio_pin_configure(led, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	ret |= gpio_pin_configure(button, SW0_GPIO_PIN, SW0_GPIO_FLAGS);
+
+	if (ret!=0)
+		return false;
+
+	ret = gpio_pin_interrupt_configure(button,SW0_GPIO_PIN,GPIO_INT_EDGE_TO_ACTIVE);
+
+	if (ret!=0)
+		return false;
+	
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(SW0_GPIO_PIN));
+	gpio_add_callback(button, &button_cb_data);
+
+	return true; 
 }
