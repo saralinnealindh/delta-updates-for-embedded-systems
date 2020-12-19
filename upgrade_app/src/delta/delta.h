63e9c199cb18b35d7e47aed7eab41961448beea2
Linnéa Lindh <linnea.lindh@endian.se>
2020-12-19 20:07:45 +0100

    cleaned code up a bit

---
+++ upgrade_app/src/delta/delta.h
@@ -0,0 +1,88 @@
+#ifndef DELTA_H
+#define DELTA_H
+
+#include <zephyr.h>
+#include <sys/util.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/flash.h>
+#include <storage/flash_map.h>
+#include <dfu/mcuboot.h>
+#include <power/reboot.h>
+#include "../detools/detools.h"
+
+/* IMAGE OFFSETS AND SIZES */
+#define PRIMARY_OFFSET FLASH_AREA_OFFSET(image_0)
+#define PRIMARY_SIZE FLASH_AREA_SIZE(image_0)
+#define SECONDARY_OFFSET FLASH_AREA_OFFSET(image_1)
+#define SECONDARY_SIZE FLASH_AREA_SIZE(image_1)
+#define STORAGE_OFFSET FLASH_AREA_OFFSET(storage)
+#define STORAGE_SIZE FLASH_AREA_SIZE(storage)
+
+/* PATCH HEADER SIZE */
+#define HEADER_SIZE 0x18
+
+/* Error codes. */
+#define DELTA_OK                                          0
+#define DELTA_SLOT1_OUT_OF_MEMORY                        28
+#define DELTA_READING_PATCH_ERROR						 29
+#define DELTA_READING_SOURCE_ERROR						 30
+#define DELTA_WRITING_ERROR			                     31
+#define DELTA_SEEKING_ERROR								 32
+#define DELTA_CASTING_ERROR                              33
+#define DELTA_INVALID_BUF_SIZE							 34
+#define DELTA_CLEARING_ERROR							 35
+#define DELTA_NO_FLASH_FOUND							 36
+
+/* FLASH MEMORY AND POINTERS TO CURRENT LOCATION OF BUFFERS AND END OF IMAGE AREAS.
+ * - "Patch" refers to the area containing the patch image. 
+ * - "From" refers to the area containing the source image. 
+ * - "To" refers to the area where the target image is to be placed.
+ * */
+struct flash_mem {
+	const struct device *device;
+	off_t patch_current; 
+	off_t patch_end; 
+	off_t from_current; 
+	off_t from_end; 
+	off_t to_current; 
+	off_t to_end; 
+};
+
+/* FUNCTION DECLARATIONS */
+
+/**
+ * Checks if there is patch in the patch partition 
+ * and applies that patch if it exists. Then restarts
+ * the device and boots from the new image. 
+ *
+ * @param[in] flash the devices flash memory. 
+ *
+ * @return zero(0) if no patch or a negative error
+ * code.
+ */
+int delta_check_and_apply(struct flash_mem *flash);
+
+/**
+ * Functiong for reading the metadata from the patch and 
+ * changing the header to mark that the patch has been 
+ * applied.
+ *
+ * @param[in] flash the devices flash memory. 
+ *
+ * @return zero(0) if not patch, a negative value (0>)
+ * representing an error, or a positive value (0<)
+ * representing the patch size. 
+ */
+int delta_read_patch_header(struct flash_mem *flash);
+
+/**
+ * Get the error string for given error code.
+ *
+ * @param[in] Error code.
+ *
+ * @return Error string.
+ */
+const char *delta_error_as_string(int error);
+
+#endif
--- upgrade_app/src/main.c
+++ upgrade_app/src/main.c
@@ -1,225 +1,93 @@
-#include "main.h"
+#include <zephyr.h>
+#include <sys/util.h>
+#include <sys/printk.h>
+#include <drivers/gpio.h>
+#include "delta/delta.h"
+
+/* BUTTON */
+#define SW0_NODE	DT_ALIAS(sw0)
+
+#if DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#define SW0_GPIO_LABEL	DT_GPIO_LABEL(SW0_NODE, gpios)
+#define SW0_GPIO_PIN	DT_GPIO_PIN(SW0_NODE, gpios)
+#define SW0_GPIO_FLAGS	(GPIO_INPUT | DT_GPIO_FLAGS(SW0_NODE, gpios))
+#else
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#define SW0_GPIO_LABEL	""
+#define SW0_GPIO_PIN	0
+#define SW0_GPIO_FLAGS	0
+#endif
+
+static struct gpio_callback button_cb_data;
+static bool btn_flag;
+
+/* LED */
+#define SLEEP_TIME_MS   1000 //BLINKING SPEED
+
+#define LED0_NODE DT_ALIAS(led0)
+#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#else
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led0 devicetree alias is not defined"
+#define LED0	""
+#define PIN	0
+#define FLAGS	0
+#endif
+
+//Initiating button and led
+static int config_devices(const struct device *button, 
+					const struct device *led);
+
+//Responds to button 1 being pressed 
+void button_pressed(const struct device *dev, 
+					struct gpio_callback *cb,
+		    		uint32_t pins);
 
 /* MAIN FUNCTION */
 void main(void)
 {
 	const struct device *led,*button;
 	struct flash_mem *flash_pt;
-	int ret,length,patch_size;
-	bool led_is_on;
+	bool led_is_on = true;
+	int ret;
 
 	led = device_get_binding(LED0);
 	button = device_get_binding(SW0_GPIO_LABEL);
 	flash_pt = k_malloc(sizeof(struct flash_mem));
 	flash_pt->device = device_get_binding(DT_CHOSEN_ZEPHYR_FLASH_CONTROLLER_LABEL);
 
-	length = 8;
-	led_is_on = true;
-	btn_flag = false; 
-
-	unsigned char buf[length];
-
 	if (!led||!button||!flash_pt->device)
 		return;
 
-	if (config_devices(flash_pt->device,button,led))
-		return;
-
-	if(init_flash_mem(flash_pt))
+	if (config_devices(button,led))
 		return;
 	
-	printk("Hello World from %s on %s!\n",MCUBOOT_BLINKY2_FROM, CONFIG_BOARD);
-
-	ret = clear_slot1(flash_pt->device);
-
-	if (ret) 
-		printk("Something went wrong :( %X \n",ret);
-
 	/*Main loop*/
-	while (1) {
+	while (1) 
+	{
+		/* turn light on/off */
 		gpio_pin_set(led, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
 
 		if(btn_flag)
 		{
-			ret = flash_read(flash_pt->device,SECONDARY_OFFSET,buf,length);
+			ret = delta_check_and_apply(flash_pt);
 
-			if(ret)
+			if(ret){
+				printk("%s",delta_error_as_string(ret));
 				return;
-
-			patch_size = read_patch_header(flash_pt);
-
-			printk("Size: %X\n",patch_size);
-			printk("Flash location: %d\n",flash_pt->device);
-
-			if(patch_size>0) //if patch exists
-			{
-				printk("Appling patch... \n");
-				ret = detools_apply_patch_callbacks(du_flash_from_read,
-											du_flash_seek,
-											du_flash_patch_read,
-											(size_t) patch_size,
-											du_flash_write,
-											flash_pt);
-				if(ret<=0)
-					printk("Error: %d \n",ret);
-				
-				if(boot_request_upgrade(BOOT_UPGRADE_PERMANENT))
-					return;
-				
-				sys_reboot(SYS_REBOOT_COLD);
 			}
-
+				
 			btn_flag = false;
 		}
 	}
 }
 
-static int du_flash_write(void *arg_p,
-					const uint8_t *buf_p,
-					size_t size)
-{
-	struct flash_mem *flash; 
-    flash = (struct flash_mem *)arg_p;
-
-	if(!flash)
-		return 0x18;//couldnt convert to flash_mem
-
-	/*if (flash->to_current%4) 
-		return 0x12; //if word not aligned*/
-
-	if (flash_write_protection_set(flash->device, false)) 
-		return 0x13; //write protection could not be removed
-
-	if (flash_write(flash->device, flash->to_current,buf_p, size)) 
-		return 0x14; //writing could not be done
-
-	if (flash_write_protection_set(flash->device, true))
-		return 0x15; //writing protection could not be set again
-
-	flash->to_current += (off_t) size;
-
-	if (flash->to_current>=flash->to_end)
-		return 0x1; //out of memory
-
-	return 0;
-}
-
-static int du_flash_from_read(void *arg_p,
-					uint8_t *buf_p,
-					size_t size)
-{
-	struct flash_mem *flash; 
-    flash = (struct flash_mem *)arg_p;
-
-	if(!flash)
-		return 0x28; //couldnt convert to flash_mem
-
-	if (size <= 0) 
-		return 0x26; //invalid buff size
-
-	if(flash_read(flash->device,flash->from_current,buf_p,size))
-		return 0x27; //could not read
-
-	flash->from_current += (off_t) size;
-
-	if (flash->from_current>=flash->from_end)
-		return 0x21; //out of memory
-
-	return 0;
-}
-
-static int du_flash_patch_read(void *arg_p,
-					uint8_t *buf_p,
-					size_t size)
-{
-	struct flash_mem *flash; 
-    flash = (struct flash_mem *)arg_p;
-
-	if(!flash)
-		return 0x38; //couldnt convert to flash_mem
-
-	if (size <= 0)
-		return 0x36; //invalid buff size
-
-	if (flash_read(flash->device,flash->patch_current,buf_p,size))
-		return 0x37; //could not read 
-	
-	flash->patch_current += (off_t) size;
-
-	if (flash->patch_current>=flash->patch_end)
-		return 0x31; //out of memory
-	
-	return 0;
-}
-
-static int du_flash_seek(void *arg_p,int offset)
-{
-	struct flash_mem *flash;
-    flash = (struct flash_mem *)arg_p;
-
-	if(!flash)
-		return 0x48; //couldnt convert to flash_mem
-
-	flash->from_current += offset;
-
-	if (flash->from_current>=flash->from_end)
-		return 0x41; //out of memory
-
-	return 0;
-}
-
-static int read_patch_header(struct flash_mem *flash)
-{
-	int header_len, word_len, size;
-	word_len = 0x8;
-	header_len = HEADER_SIZE;
-	unsigned char *buf_p,*buf_end,buf[header_len];
-	unsigned char word_buf[] = {'N','E','W','P','A','T','C','H'};
-	unsigned char new_word[] = {'F','F','F','F','F','F','F','F'};
-
-	if(flash_read(flash->device,STORAGE_OFFSET,buf,header_len))
-		return -0x7; //could not read flash
-
-	if(strncmp(buf,word_buf,word_len))
-		return -1; //no new patch
-	
-	buf_p=&buf[word_len];
-	buf_end=&buf[header_len];
-
-	size = (int) strtol(buf_p,buf_end,header_len-word_len);
-
-	if (flash_write_protection_set(flash->device, false)) 
-		return -0x3; //write protection could not be removed
-
-	if (flash_write(flash->device, STORAGE_OFFSET,new_word, word_len)) 
-		return -0x4; //writing could not be done
-
-	if (flash_write_protection_set(flash->device, true))
-		return -0x5; //writing protection could not be set again
-
-	return size;
-}
-
-static int init_flash_mem(struct flash_mem *flash)
-{
-	if(!flash)
-		return -1;
-
-	flash->from_current = PRIMARY_OFFSET;
-	flash->from_end = flash->from_current + PRIMARY_SIZE;
-
-	flash->to_current = SECONDARY_OFFSET;
-	flash->to_end = flash->to_current + SECONDARY_SIZE;
-
-	flash->patch_current = STORAGE_OFFSET + HEADER_SIZE;
-	flash->patch_end = flash->patch_current + STORAGE_SIZE;
-
-	return 0;
-}
-
-static int config_devices(const struct device *flash, const struct device *button, const struct device *led)
+static int config_devices(const struct device *button, const struct device *led)
 {
 	int ret; 
 	ret = gpio_pin_configure(led, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
@@ -234,21 +102,9 @@ static int config_devices(const struct device *flash, const struct device *butto
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(SW0_GPIO_PIN));
 	gpio_add_callback(button, &button_cb_data);
 
+	btn_flag = false; 
+
 	return 0; 
-}
-
-static int clear_slot1(const struct device *flash) 
-{
-	if (flash_write_protection_set(flash, false)) 
-		return 0x53; //write protection could not be removed
-
-	if (flash_erase(flash,SECONDARY_OFFSET,SECONDARY_SIZE)) 
-		return 0x54; //writing could not be done
-
-	if (flash_write_protection_set(flash, true))
-		return 0x55; //writing protection could not be set again
-
-	return 0;
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
@@ -257,7 +113,3 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb,
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 	btn_flag = true;
 }
-
-
-
-
